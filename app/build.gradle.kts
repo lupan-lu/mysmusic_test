415ef0f41d317d552eed7c9df41fde30ac653d0d
LuPan <1027629009@qq.com>
2025-04-16 16:25:13 +0800

    初始化提交

---
+++ app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.music"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.music"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+}
